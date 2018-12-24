2d2e9f601a9a07385f536ca3d52f1c399cb7a088
3A532017 <s3a532017@student.ncut.edu.tw>
2018-12-25 00:31:01 +0800

    增加任務 修正視圖

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 0106
- * Date: 2018/12/25
- * Time: 上午 12:14
- */
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
